1ab4917cc93ad0f9f409ebb4faaa52b4ba440794
nerett <pt-mvb@inbox.lv>
2024-09-16 21:57:44 +0300

    updated helloworld.c

--- 3-OpenMP-introduction/helloworld.c
+++ 3-OpenMP-introduction/helloworld.c
@@ -4,8 +4,9 @@
 int main()
 {
     #pragma omp parallel
+    {
         printf("OpenMP thread %d/%d: Hello World!\n", omp_get_thread_num(), omp_get_num_threads());
-    #pragma endregion
+    }
 
     return 0;
 }
