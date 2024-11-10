8e003a25401a8536caadd9f6735664d319d1edf8
nerett <pt-mvb@inbox.lv>
2024-11-11 00:21:50 +0300

    refactored error messages, added time calculation

--- 6-OpenCL-additional/cl-matrix.c
+++ 6-OpenCL-additional/cl-matrix.c
@@ -25,25 +25,24 @@
 
 cl_device_id create_device()
 {
-    cl_platform_id platform = {0};
     cl_device_id device = {0};
     cl_int err = 0;
 
     cl_uint num_platforms = 0;
     cl_int status = clGetPlatformIDs(0, NULL, &num_platforms);
     if (status != CL_SUCCESS || num_platforms <= 0) {
-        perror("Couldn't find any platforms");
+        perror("clGetPlatformIDs");
         exit(EXIT_FAILURE);
     }
 
     cl_platform_id* platforms = (cl_platform_id*)calloc(num_platforms, sizeof(cl_platform_id));
     status = clGetPlatformIDs(num_platforms, platforms, NULL);
     if (status != CL_SUCCESS) {
-        perror("Couldn't get platforms");
+        perror("clGetPlatformIDs");
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i < num_platforms; ++i) {
+    for (cl_uint i = 0; i < num_platforms; ++i) {
         err = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_GPU, 1, &device, NULL);
         if(err == CL_SUCCESS) {
             break;
@@ -55,10 +54,10 @@ cl_device_id create_device()
     cl_bool device_ok = CL_FALSE;
     clGetDeviceInfo(device, CL_DEVICE_AVAILABLE, sizeof(device_ok), &device_ok, NULL);
     if (device_ok == CL_TRUE) {
-
         char name[STR_LEN] = "";
         char vendor[STR_LEN] = "";
         char version[STR_LEN] = "";
+
         clGetDeviceInfo(device, CL_DEVICE_NAME, sizeof(name), name, NULL);
         clGetDeviceInfo(device, CL_DEVICE_VENDOR, sizeof(vendor), vendor, NULL);
         clGetDeviceInfo(device, CL_DEVICE_VERSION, sizeof(version), version, NULL);
@@ -73,49 +72,47 @@ cl_device_id create_device()
     return device;
 }
 
-cl_program build_program(cl_context ctx, cl_device_id dev, const char* filename)
+cl_program build_program(cl_context context, cl_device_id device, const char* filename)
 {
-    cl_program program = {0};
-    FILE *program_handle = NULL;
-    char *program_buffer = NULL, *program_log = NULL;
-    size_t program_size = 0, log_size = 0;
-    cl_int err = 0;
+    cl_int err = CL_SUCCESS;
 
-    program_handle = fopen(filename, "r");
-    if(program_handle == NULL) {
-        perror("Couldn't find the program file");
+    FILE* program_handle = fopen(filename, "r");
+    if(!program_handle) {
+        perror("fopen");
         exit(EXIT_FAILURE);
     }
 
     fseek(program_handle, 0, SEEK_END);
-    program_size = ftell(program_handle);
+    size_t program_size = ftell(program_handle);
     rewind(program_handle);
 
-    program_buffer = (char*)calloc(program_size + 1, sizeof(char));
+    char* program_buffer = (char*)calloc(program_size + 1, sizeof(char));
     size_t num_read = fread(program_buffer, sizeof(char), program_size, program_handle);
     if(num_read != program_size) {
-        fclose(program_handle);
+        perror("fread");
 
-        perror("Couldn't read the program file");
+        fclose(program_handle);
         exit(EXIT_FAILURE);
     }
 
     fclose(program_handle);
 
-    program = clCreateProgramWithSource(ctx, 1, (const char**)&program_buffer, &program_size, &err);
+    cl_program program = clCreateProgramWithSource(context, 1, (const char**)&program_buffer, &program_size, &err);
     if(err != CL_SUCCESS) {
-        perror("Couldn't create the program");
+        perror("clCreateProgramWithSource");
         exit(EXIT_FAILURE);
     }
     free(program_buffer);
 
     err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
     if(err != CL_SUCCESS) {
-        clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-        program_log = (char*)calloc(log_size + 1, sizeof(char));
+        size_t log_size = 0;
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+        char* program_log = (char*)calloc(log_size + 1, sizeof(char));
 
-        clGetProgramBuildInfo(program, dev, CL_PROGRAM_BUILD_LOG, log_size + 1, program_log, NULL);
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size + 1, program_log, NULL);
         printf("%s\n", program_log);
+
         free(program_log);
         exit(EXIT_FAILURE);
     }
@@ -142,7 +139,7 @@ int main()
     cl_device_id device = create_device();
     cl_context context = clCreateContext(NULL, 1, &device, NULL, NULL, &err);
     if(err != CL_SUCCESS) {
-        perror("Couldn't create a context");
+        perror("clCreateContext");
         exit(EXIT_FAILURE);
     }
 
@@ -152,19 +149,19 @@ int main()
     cl_mem device_B = clCreateBuffer(context, CL_MEM_READ_ONLY|CL_MEM_COPY_HOST_PTR, MATRIX_DIM * MATRIX_DIM * sizeof(long), B, &err);
     cl_mem device_C = clCreateBuffer(context, CL_MEM_READ_WRITE|CL_MEM_COPY_HOST_PTR, MATRIX_DIM * MATRIX_DIM * sizeof(long), C, &err);
     if(err != CL_SUCCESS) {
-        perror("Couldn't create a buffer");
+        perror("clCreateBuffer");
         exit(EXIT_FAILURE);
     };
 
-    cl_command_queue queue = clCreateCommandQueue(context, device, 0, &err); //!TODO check platform version
+    cl_command_queue queue = clCreateCommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err); //!TODO check platform version
     if(err != CL_SUCCESS) {
-        perror("Couldn't create a command queue");
+        perror("clCreateCommandQueue");
         exit(EXIT_FAILURE);
     };
 
     cl_kernel kernel = clCreateKernel(program, KERNEL_FUNC, &err);
     if(err != CL_SUCCESS) {
-        perror("Couldn't create a kernel");
+        perror("clCreateKernel");
         exit(EXIT_FAILURE);
     };
 
@@ -174,30 +171,40 @@ int main()
     err |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &device_C);
     err |= clSetKernelArg(kernel, 3, sizeof(int), &dim);
     if(err != CL_SUCCESS) {
-        perror("Couldn't create a kernel argument");
+        perror("clSetKernelArg");
         exit(EXIT_FAILURE);
     }
 
+    printf("Running mul_matrix() on device\n");
+
     size_t global_size[2] = {MATRIX_DIM, MATRIX_DIM};
     size_t local_size[2] = {DEVICE_LOCAL_SIZE, DEVICE_LOCAL_SIZE};
+    cl_event event = {0};
 
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_size, local_size, 0, NULL, NULL);
+    err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_size, local_size, 0, NULL, &event);
     if(err != CL_SUCCESS) {
-        perror("Couldn't enqueue the kernel");
+        perror("clEnqueueNDRangeKernel");
         exit(EXIT_FAILURE);
     }
 
     err = clEnqueueReadBuffer(queue, device_C, CL_TRUE, 0, MATRIX_DIM * MATRIX_DIM * sizeof(long), C, 0, NULL, NULL);
     if(err != CL_SUCCESS) {
-        perror("Couldn't read the buffer");
+        perror("clEnqueueReadBuffer");
         exit(EXIT_FAILURE);
     }
+
+    clFinish(queue);
+
+    cl_ulong start = 0, end = 0;
+    clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start, NULL);
+    clGetEventProfilingInfo(event, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end, NULL);
+
+    printf("\n");
+    printf("Multiplication time: %lf\n", ((double)end - (double)start)/1e9);
 
     printf("hash(A) = %x\n", hash_matrix(A, MATRIX_DIM));
     printf("hash(B) = %x\n", hash_matrix(B, MATRIX_DIM));
     printf("hash(C) = %x\n", hash_matrix(C, MATRIX_DIM));
-
-    clFinish(queue);
 
     clReleaseKernel(kernel);
     clReleaseMemObject(device_A);
